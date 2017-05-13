9b3e114c3988ec4d0403660020abbb0074af12b2
Andrey Reinwald <andreinwald@gmail.com>
2017-05-13 15:34:30 +0300

    First version of index.php with result

--- public/index.php
+++ public/index.php
@@ -3,22 +3,23 @@ define('APP_START_TIME', microtime(true));
 chdir(dirname(__DIR__));
 require_once 'vendor/autoload.php';
 
-echo 'App speed about ' . round(1 / (microtime(true) - APP_START_TIME)) . ' RPS <br>';
+// Create Request
+$request = \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
 
-// Build Request
-$input = json_decode(file_get_contents('php://input'), true) ?: [];
-$parsedBody = array_merge($_POST, $input);
-$request = \Zend\Diactoros\ServerRequestFactory::fromGlobals(null, null, $parsedBody);
-
-// Dispatch Middlewares
-$dispatcher = new \Middleland\Dispatcher(require_once 'app/middlewares.php');
+// Dispatch Middleware
+$dispatcher = new \Middleland\Dispatcher(require_once 'app/middleware.php');
+ob_start(); // catch any error and other text
 $response = $dispatcher->dispatch($request);
+$log = ob_get_clean();
+
 
 // Sending response
 $statusCode = $response->getStatusCode();
 $reasonPhrase = $response->getReasonPhrase();
 $protocolVersion = $response->getProtocolVersion();
 header("HTTP/{$protocolVersion} $statusCode $reasonPhrase");
+
+// Sending headers
 foreach ($response->getHeaders() as $name => $values) {
     if (strtolower($name) === 'set-cookie') {
         foreach ($values as $cookie) {
@@ -28,7 +29,13 @@ foreach ($response->getHeaders() as $name => $values) {
     }
     header(sprintf('%s: %s', $name, $response->getHeaderLine($name)));
 }
+
+// Prepare body
 $body = $response->getBody();
 if ($body) {
-    echo $body->__toString();
+    $body = $body->__toString();
 }
+
+header('Execution-Time: ' . (microtime(true) - APP_START_TIME) * 1000);
+
+echo $body;

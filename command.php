8f998c8dc2e4b27b0677c1e2f8101aed97388274
Andrey Reinwald <andreinwald@gmail.com>
2017-09-09 09:29:00 +0300

    Set up Injectable in console

--- command.php
+++ command.php
@@ -2,6 +2,12 @@
 define('APP_START_TIME', microtime(true));
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Set up Container
+$services = require_once __DIR__ . '/config/services.php';
+$container = \Injectable\Factories\LeagueFactory::fromConfig($services);
+\Injectable\ContainerSingleton::setContainer($container);
+
+// Run Console
 $cli = new \Symfony\Component\Console\Application();
 $cli->addCommands(require __DIR__ . '/config/commands.php');
 $cli->run();

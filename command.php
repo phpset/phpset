e3f78fc0eef42447ba6499021f127e85fe5d11f4
Andrey Reinwald <andreinwald@gmail.com>
2017-09-16 22:57:00 +0300

    Added env

--- command.php
+++ command.php
@@ -1,10 +1,14 @@
 <?php
 define('APP_START_TIME', microtime(true));
+define('APP_DIR', __DIR__);
+
 require_once __DIR__ . '/vendor/autoload.php';
 
+
 // Set up Container
-$services = require_once __DIR__ . '/config/services.php';
-$container = \Injectable\Factories\LeagueFactory::fromConfig($services);
+$env = require __DIR__ . '/config/env.php';
+$services = require __DIR__ . '/config/services.php';
+$container = \Injectable\Factories\LeagueFactory::fromConfig($services, [$env]);
 \Injectable\ContainerSingleton::setContainer($container);
 
 // Run Console

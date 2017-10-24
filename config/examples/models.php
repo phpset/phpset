7177a640bd88d9f9c8237643e7cc0fc8e38c1444
Andrey Reinwald <andreinwald@gmail.com>
2017-10-24 12:02:53 +0300

    Improced configs

---
+++ config/examples/models.php
@@ -0,0 +1,3 @@
+<?php return [
+    'vids' => \App\Vids\VidsModel::class,
+];
--- config/examples/services.php
+++ config/examples/services.php
@@ -1,3 +1,9 @@
 <?php return [
-    'guzzleClient' => new \GuzzleHttp\Client(),
+    'mysql' => function ($env) {
+        $connection = \PdoModel\PdoFactory::createConnection($env['mysql']);
+        $models = include 'models.php';
+        return new \App\Base\ModelLocator($connection, $models);
+    },
+
+    'curlRequest' => new \App\Request\Curl(),
 ];

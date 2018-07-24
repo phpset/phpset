841d0a710ec00f21870ea02c0d7be4f5565a123a
Andrey Reinwald <andreinwald@gmail.com>
2018-07-24 13:37:14 +0300

    Allow change Response in Controller

--- app/Base/HttpController.php
+++ app/Base/HttpController.php
@@ -3,13 +3,9 @@ declare(strict_types=1);
 
 namespace App\Base;
 
-use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-/**
- * @property RequestInterface request
- * @property ResponseInterface response
- */
 abstract class HttpController
 {
     use Injectable;
@@ -17,7 +13,7 @@ abstract class HttpController
     protected $request;
     protected $response;
 
-    public function __construct(RequestInterface $request, ResponseInterface &$response)
+    public function __construct(ServerRequestInterface $request, ResponseInterface &$response)
     {
         $this->request = $request;
         $this->response = &$response;
